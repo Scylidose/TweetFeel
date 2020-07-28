18d0472f3ddd7d754c2c94ac8c92985db31caccb
Jordi saleilles <jordisaleilles@macbook-pro-de-jordi.home>
2020-07-28 08:34:20 -0400

    Added list of cryptocurrency symbols.

--- Predict Stock Prices/index.py
+++ Predict Stock Prices/index.py
@@ -7,14 +7,35 @@ from flask import (Flask, url_for, render_template,  make_response,
                    redirect, request, g, session, Response, jsonify)
 
 #from src.main import predict_stock
+import yfinance as yf
+import pandas as pd
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+data_path = 'data/'
 
 @app.route('/')
 def accueil():
+    crypto = pd.read_csv(data_path+'cryptocurrencies.csv')['Cryptocurrency'].to_list()
+
+    currency = ['USD', 'CAD', 'EUR']
+
+    print(crypto)
+
     return render_template("accueil.html")
+
+@app.route('/load_currency', methods=['POST'])
+def load_currency():
+
+    currency = request.form["currency"]
+
+    msft = yf.Ticker(currency)
+
+    data_file_path = data_path + currency + '.csv'
+
+    max_range_batcad = msft.history(period="max")
+    max_range_batcad.to_csv(data_file_path, index = False)
 
 
 '''

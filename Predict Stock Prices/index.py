af37facb3151cb3ca1292cd9ea19377d6977442e
Jordi saleilles <jordisaleilles@macbook-pro-de-jordi.home>
2020-08-02 11:50:01 -0400

    Reduced project features to focus on one crypto, refactored code.

--- Predict Stock Prices/index.py
+++ Predict Stock Prices/index.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # coding: utf8
 
-import yfinance as yf
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import r2_score
 
 from datetime import date
 
@@ -43,102 +41,97 @@ def fetch_cryptocurrency(crypto_sel, currency_sel):
 
     return (crypto_json, currency_json)
 
+def save_plot(data, first_data, second_data, color, legend, title, save):
+    fig, ax = plt.subplots()
+
+    plt.plot(data.iloc[:, 0], data.iloc[:,first_data], color[0])
+    if second_data is not None:
+        plt.plot(data.iloc[:,second_data], color[1])
+        plt.legend(legend)
+
+    plt.title(title)
+    plt.xticks(rotation='vertical')
+
+    every_nth = 3
+    for n, label in enumerate(ax.xaxis.get_ticklabels()):
+        if n % every_nth != 0:
+            label.set_visible(False)
+
+    plt.savefig('static/img/'+save+'.png', transparent=True)
+
 @app.route('/')
 def accueil():
+    check_pred_today = pd.read_csv("src/results/today_score.csv")
+
     today = date.today()
     today_date = today.strftime("%B %d, %Y")
-
+    
     cryptocurrency = "BAT-CAD"
+    data = main.load_data(data_path, cryptocurrency)
 
-    if('cryptocurrency' in request.args):
-        cryptocurrency = request.args['cryptocurrency']
-        crypto_sel = cryptocurrency.split("-")[0]
-        currency_sel = cryptocurrency.split("-")[1]
+    if(check_pred_today.iloc[0, 0] != today_date):
+        data.iloc[:, 0] = pd.to_datetime(data.iloc[:,0],format='%Y-%m-%d').dt.strftime('%d-%m').tolist()
+
+        data_sel = data.iloc[-30:,].reset_index(drop=True)
+
+        save_plot(data_sel, 1, 4, ['-b', '-r'], ["Open Value", "Close Value"],
+        "Open and close for " + cryptocurrency + " price per day (Last 30 Days) ", "open_close")
+
+        save_plot(data_sel, 2, 3, ['-c', '-m'], ["Highest", "Lowest"],
+        "Highest and Lowest for " + cryptocurrency + " price per day (Last 30 Days)", "high_low")
+
+        save_plot(data_sel, 6, None,['-g'], None, "Volume value for " + cryptocurrency + " per day (Last 30 Days)", "volume")
+
+        prediction_result = main.predict_stock(data_path, cryptocurrency)
+
+        (unscaled_y_pred, unscaled_y_test, predicted_close_today) = prediction_result[0]
+
+        accuracy = prediction_result[1]
+
+        prediction = predicted_close_today[0]
+
+        today_pred = {'Date': [], 'Prediction': [], 'Accuracy': []}
+
+        today_pred['Date'] = today_date
+        today_pred['Prediction'] = prediction
+        today_pred['Accuracy'] = accuracy
+
+        today_pred = pd.DataFrame(today_pred, index=[0])
+
+        today_pred.to_csv(r'src/results/today_score.csv', index=False)
+
+
+        plt.figure()
+        data_sel = -len(unscaled_y_test)
+
+        fig, ax = plt.subplots()
+        plt.plot(data.iloc[data_sel:,0], unscaled_y_test[:,0], '-b', label='Real Close Value')
+        plt.plot(unscaled_y_pred[:,0], '-r', label='Predicted Close Value')
+        plt.legend(['Real Close Value', 'Predicted Close Value'])
+        plt.title("Predicted and Real close value for " + cryptocurrency + " (Last Year)")
+        plt.xticks(rotation='vertical')
+
+        every_nth = 10
+        for n, label in enumerate(ax.xaxis.get_ticklabels()):
+            if n % every_nth != 0:
+                label.set_visible(False)
+        plt.savefig('static/img/year_accuracy.png', transparent=True)
+
     else:
-        crypto_sel = "BAT"
-        currency_sel = "CAD"
+        prediction = check_pred_today.iloc[0, 1]
+        accuracy = check_pred_today.iloc[0, 2]
 
-    curr = yf.Ticker(cryptocurrency)
+    diff = (data.iloc[-2, 4] - prediction) / data.iloc[-2, 4] * 100
+
+    if data.iloc[-2, 4] > prediction:
+        diff = -diff
     
-    main.fetch_currency(data_path, cryptocurrency)
-
-    data = main.load_data(data_path, cryptocurrency).iloc[-30:,].reset_index(drop=True)
-
-    plt.figure()
-
-    plt.plot(data.iloc[:,0], '-b')
-    plt.plot(data.iloc[:,3], '-r')
-    plt.legend(["Open Value", "Close Value"])
-    plt.title("Open and close for " + crypto_sel + " price per day (Last 30 Days) ")
-    plt.xticks([])
-
-    plt.savefig('static/img/open_close.png', transparent=True)
-
-    plt.figure()
-    plt.plot(data.iloc[:,1], '-c')
-    plt.plot(data.iloc[:,2], '-m')
-    plt.legend(["Highest", "Lowest"])
-    plt.title("Highest and Lowest for " + crypto_sel + " price per day (Last 30 Days)")
-    plt.xticks([])
-
-    plt.savefig('static/img/high_low.png', transparent=True)
-    
-    plt.figure()
-    plt.plot(data.iloc[:,4])
-    plt.title("Volume value for " + crypto_sel + " per day (Last 30 Days)")
-    plt.xticks([])
-
-    plt.savefig('static/img/volume.png', transparent=True)
-
-    (crypto_json, currency_json) = fetch_cryptocurrency(crypto_sel, currency_sel)
-
-    prediction_result = main.predict_stock(data_path, cryptocurrency)
-
-    (unscaled_y_pred, unscaled_y_test, predicted_close_today) = prediction_result[0]
-
-    (unscaled_X_pred_future, unscaled_y_pred_future) = prediction_result[1]
-
-    prediction = predicted_close_today[0]
-
-    plt.figure()
-    plt.plot(unscaled_y_test, label='Real Close Value')
-    plt.plot(unscaled_y_pred, label='Predicted Close Value')
-    plt.legend(['Real Close Value', 'Predicted Close Value'])
-    plt.title("Predicted and Real close value for " + crypto_sel + " (Last Year)")
-    plt.xticks([])
-
-    plt.figure()
-    plt.plot(unscaled_X_pred_future, label='Predicted Open Value')
-    plt.plot(unscaled_y_pred_future, label='Predicted Close Value')
-    plt.legend(['Predicted Open Value', 'Predicted Close Value'])
-    plt.title("Predicted open and Predicted close value for " + crypto_sel + " (Next 30 days)")
-    plt.xticks([])
-
-    plt.savefig('static/img/future_pred.png', transparent=True)
-
     return render_template("accueil.html", 
-                            date=today_date, crypto=crypto_json, currency=currency_json, 
-                            desc=curr.info['description'],
-                            accuracy=round(r2_score(unscaled_y_test,unscaled_y_pred)*100, 3), 
-                            open = data.iloc[-1, 0], close=prediction.round(decimals=5),
+                            date=today_date, 
+                            accuracy=accuracy, diff=round(diff,3),
+                            open = data.iloc[-1, 1], close=prediction.round(decimals=5),
                             open_close ='/static/img/open_close.png', high_low='/static/img/high_low.png', volume='/static/img/volume.png',
-                            year_accuracy='/static/img/year_accuracy.png', future_pred='/static/img/future_pred.png')
-
-@app.route('/load_currency', methods=['POST'])
-def load_currency():
-
-    crypto = request.form.get('crypto')
-    currency = request.form.get('currency')
-
-    cryptocurrency = crypto + "-" + currency
-    curr = yf.Ticker(cryptocurrency)
-    
-    data_file_path = data_path + cryptocurrency + '.csv'
-
-    max_range_curr = curr.history(period="max")
-    max_range_curr.to_csv(data_file_path, index = False)
-    
-    return redirect(url_for('accueil', cryptocurrency=cryptocurrency))
+                            year_accuracy='/static/img/year_accuracy.png')
 
 @app.after_request
 def add_header(response):
--- Predict Stock Prices/src/main.py
+++ Predict Stock Prices/src/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-import yfinance as yf
-
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
@@ -9,14 +7,12 @@ from sklearn import preprocessing
 import os.path
 from os import path
 
-import glob
-import random
-
 from keras.layers.core import Dense, Activation, Dropout
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
+from sklearn.metrics import r2_score
 
 from tensorflow import keras
 from keras import backend as K
@@ -25,70 +21,48 @@ from keras import backend as K
 def transf_list(lst):
     return [[el] for el in lst]
 
-
-def fetch_currency(data_path, cryptocurrency):
-    files = glob.glob(data_path+"*.csv")
-    crypto_file = data_path+'cryptocurrencies.csv'
-
-    for i in range(len(files)):
-        if(files[i] != crypto_file):
-            os.remove(files[i])
-
-    curr = yf.Ticker(cryptocurrency)
-
-    max_range_curr = curr.history(period="max")
-    max_range_curr.to_csv(data_path + cryptocurrency + '.csv', index = False)
-
 def load_data(data_path, cryptocurrency):
     data = pd.read_csv(data_path + cryptocurrency + '.csv').dropna().reset_index(drop=True)
-    data = data.drop(['Date','Dividends', 'Stock Splits'], axis=1, errors='ignore')
 
     return data
 
 def preprocess_data(data, min_max_scaler):
-    X = transf_list(data.iloc[:, 0])
-    y = data.iloc[:, [3]].values.tolist()
+    X = transf_list(data.iloc[:, 1])
+    y = data.iloc[:, [4, 5]].values.tolist()
 
+    min_max_scaler = preprocessing.MinMaxScaler()
     x_scaled = min_max_scaler.fit_transform(X)
     y_scaled = min_max_scaler.fit_transform(y)
 
-    
-    sliced_day = len(X) - 365
 
-    X_train = x_scaled[0:sliced_day]
-    X_test = x_scaled[sliced_day:]
+    train_sliced_day = round(len(X) * 0.6)
+    valid_sliced_day = round(train_sliced_day + len(X) * 0.2)
 
-    y_train = y_scaled[0:sliced_day]
-    y_test = y_scaled[sliced_day:-1]
+    X_train = x_scaled[0:train_sliced_day]
+    X_valid = x_scaled[train_sliced_day:valid_sliced_day]
+    X_test = x_scaled[valid_sliced_day:]
+
+    y_train = y_scaled[0:train_sliced_day]
+    y_valid = y_scaled[train_sliced_day:valid_sliced_day]
+    y_test = y_scaled[valid_sliced_day:]
 
     X_train = X_train.reshape(X_train.shape[0],  X_train.shape[1], 1)
+    X_valid = X_valid.reshape(X_valid.shape[0],  X_valid.shape[1], 1)
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
-    return [(X_train, y_train), (X_test, y_test)]
+    return [(X_train, y_train), (X_valid, y_valid),(X_test, y_test), min_max_scaler]
 
 def predict_stock(data_path, cryptocurrency):
-
-    fetch_currency(data_path, cryptocurrency)
 
     data = load_data(data_path, cryptocurrency)
 
     min_max_scaler = preprocessing.MinMaxScaler()
 
-    [(X_train, y_train), (X_test, y_test)] = preprocess_data(data, min_max_scaler)
+    [(X_train, y_train), (X_valid, y_valid),(X_test, y_test), min_max_scaler] = preprocess_data(data, min_max_scaler)
 
     K.clear_session()
 
-    model = get_model(X_train, cryptocurrency)
-
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
-    model.fit(X_train, y_train, epochs = 10, batch_size = 32)
-    
-    y_pred = model.predict(X_test)
-
-    y_pred_future = predict_future(model, X_test[-1], y_pred[-1])
-    unscaled_X_pred_future = min_max_scaler.inverse_transform(y_pred_future.iloc[:,0].to_list())
-    unscaled_y_pred_future = min_max_scaler.inverse_transform(y_pred_future.iloc[:,1].to_list())
+    accuracy, y_pred = get_model(X_train, y_train, X_test, y_test)
 
     K.clear_session()
 
@@ -97,40 +71,28 @@ def predict_stock(data_path, cryptocurrency):
 
     today = unscaled_y_pred[len(unscaled_y_pred) - 1]
 
-    return [(unscaled_y_pred[:-1], unscaled_y_test, today), (unscaled_X_pred_future, unscaled_y_pred_future)]
+    return [(unscaled_y_pred[:-1], unscaled_y_test, today), accuracy]
 
-def get_model(X_train, cryptocurrency):
+def get_model(X_train, y_train, X_test, y_test,
+          unit= 150, dropout_prob=0.3, opt='adam', epochs=20, batch_size=128):
+    
     model = Sequential()
 
-    model.add(LSTM(units = 100, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-    model.add(Dropout(0.3))
-    model.add(LSTM(units = 50, return_sequences = True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(units = 50))
-    model.add(Dropout(0.3))
-    model.add(Dense(units = 1, activation='linear'))
+    model.add(LSTM(units = unit, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    model.add(Dropout(dropout_prob))
+    model.add(LSTM(units = unit, return_sequences = True))
+    model.add(Dropout(dropout_prob))
+    model.add(LSTM(units = unit, return_sequences = True))
+    model.add(Dropout(dropout_prob))
+    model.add(LSTM(units = unit))
+    model.add(Dropout(dropout_prob))
+    model.add(Dense(units = 2))
 
-    return model
+    model.compile(optimizer = opt, loss = 'mean_squared_error')
 
-def predict_future(model, X_today, y_today):
-    nb_days = 30
-    y_pred_future = pd.DataFrame(data={'Predicted Open': [X_today], 'Predicted Close': [y_today]})
+    model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose=0)
+    y_pred = model.predict(X_test)
 
-    for i in range(nb_days):
-        Adj_close = y_pred_future.iloc[-1:, 1].values
-        
-        random_adj = random.randint(1, 5)
-
-        if(random.randint(0, 1) == 0):
-            Adj_close += Adj_close * (random_adj/100)
-        else:
-            Adj_close -= Adj_close * (random_adj/100)
-
-
-        X_tomorrow = np.asarray([[Adj_close]])
-        y_pred = model.predict(X_tomorrow)
-
-        y_pred_future.loc[len(y_pred_future)] = [Adj_close, y_pred[0]]
-
-    print(y_pred_future.iloc[:,1].to_list())
-    return y_pred_future
+    accuracy = round(r2_score(y_test,y_pred)*100, 3)
+    
+    return accuracy, y_pred

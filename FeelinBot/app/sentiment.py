d1ab13d17ed3eaa1e55dc27547613a18db91b5db
Jordi <jordi@naddesign.ca>
2021-08-21 20:00:58 -0400

    Added base files to manipulate DB.

---
+++ FeelinBot/app/sentiment.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+import firebase
+
+import requests
+import json
+import math
+
+app = Flask(__name__)

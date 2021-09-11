00dec52f2d747526b60869d33560c4e7724da22d
Jordi <jordi@naddesign.ca>
2021-09-11 12:11:26 -0400

    Added audio command for discord bot.

--- Discord_Bot/bot.py
+++ Discord_Bot/bot.py
@@ -1,13 +1,17 @@
 # bot.py
 import os
 import pandas as pd 
+import time 
+import json
+
+import validators
 
 from csv import writer
 from datetime import datetime
+import youtube_dl
 
 import psycopg2
 from sqlalchemy import create_engine
-
 
 import discord
 from discord.ext import commands
@@ -19,6 +23,8 @@ load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+LINKS = os.getenv('LINKS')
+VC_CHANNEL = int(os.getenv('VC_ID'))
 
 bot = commands.Bot(command_prefix='!')
 
@@ -33,17 +39,54 @@ async def on_ready():
 @bot.command(name='serena')
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def on_message(ctx, *, arg):
+
+    url = ctx.message.content
+    url = url.strip('!serena ')
+
     splitted_msg = arg.split(" ")
     text = ""
 
+    yt_links = json.loads(LINKS)
+
+    for key, value in yt_links.items():
+        if(splitted_msg[0] == key):
+            url = value
+
     if(splitted_msg[0] == 'help'):
         text = os.getenv('HELPER_MESSAGE')
+        text += "\nMes commandes audio :\n"
+
+        for key, value in yt_links.items():
+            text += key + "\n"
+        
 
         await ctx.send(text)
+
+    elif(splitted_msg[0] == 'rules34'):
+        for i in range(0, 3):
+            await ctx.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     
+    elif(validators.url(url)):
+        voice_channel = bot.get_channel(VC_CHANNEL)
+        await voice_channel.connect()
+
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        filename = await YTDLSource.from_url(url, loop=bot.loop)
+        voice_channel.play(discord.FFmpegPCMAudio(source=filename))
+
+        while voice_channel.is_playing():
+            time.sleep(1)
+        await voice_channel.disconnect()
+
     else:
         channel_id = ""
-        nb_times = int(splitted_msg[-1])
+        if(int(splitted_msg[-1])):
+            nb_times = int(splitted_msg[-1])
+        else:
+            nb_times = 1
+
         real_nb_times = nb_times
         flood_channel_id = discord.utils.get(ctx.guild.channels, name=os.getenv('FLOOD_CHANNEL_NAME'))
         if(flood_channel_id):
@@ -96,5 +139,45 @@ async def on_message(ctx, *, arg):
             else:
                 await channel.send(text)
 
+    
+    
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 bot.run(TOKEN)
